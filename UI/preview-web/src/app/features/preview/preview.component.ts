6e508bf463c8820e4647242861e3f7c68b91ecb3
Julius Bacosa <juliusbacosa@gmail.com>
2024-08-05 03:58:15 +0800

    ...

--- UI/preview-web/src/app/features/preview/preview.component.ts
+++ UI/preview-web/src/app/features/preview/preview.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NgxDocViewerModule } from 'ngx-doc-viewer';
 
 @Component({
   selector: 'app-preview',
   standalone: true,
-  imports: [],
+  imports: [
+    NgxDocViewerModule
+  ],
   templateUrl: './preview.component.html',
   styleUrl: './preview.component.scss'
 })
 export class PreviewComponent {
+  doc: string = `http://localhost:3200/word.docx`;
+  constructor() {
 
+  }
 }
